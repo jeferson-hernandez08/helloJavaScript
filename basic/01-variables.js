61ff773614c699902451211812b82b3be6ff7b69
Jeferson <jefer.hernandez1@gmail.com>
2025-01-08 17:38:52 -0500

    Hello JavaScript

---
+++ basic/01-variables.js
@@ -0,0 +1,17 @@
+// Las variables es la posibilidad de guardar datos en memoria valores y podamos trabajar con ellos o la posibilidad de guardar un valor o dato a un objeto.
+// En javascript tenemos tres tipos de variables. 
+
+// var: Fué la primera forma y original de declarar una variable en JS. 
+var helloWorld = "¡Hola, JavaScript!"
+console.log(helloWorld)    
+
+helloWorld = "¡Hola de nuevo, JavaScript!"      // Podemos transformar la variable 
+console.log(helloWorld)
+
+// Esta forma No es la recomendada de declarar variables ya que var tiene muchos problemas hay problemas de definicion de variabnles con var. 
+// La forma mas recomendada de declarar o deficicion de variables es con let y const.
+
+
+// let
+
+// const
