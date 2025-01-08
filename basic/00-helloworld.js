04b7ff4161ce0078fd212c195dc5b77a97320046
jeferson-hernandez08 <112195413+jeferson-hernandez08@users.noreply.github.com>
2025-01-08 17:09:04 -0500

    Add files via upload

---
+++ basic/00-helloworld.js
@@ -0,0 +1,23 @@
+// Comentario simple. 
+/*
+Esto es un 
+comentario en 
+varias lineas
+*/
+
+// Console.log 
+console.log("!Hola, JavaScript!");           // Recordemos que JS ya es necesodad de colocar ;
+console.log('!Hola, JavaScript!')           // Segunda forma: Podemos usar comillas simples tambien
+console.log(`!Hola, JavaScript!`)           // Tercera forma
+
+// Console.log con operaciones 
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5 * 2)
+console.log(5 / 2)
+console.log(5 % 2)
+console.log(5 ** 2)
+
+
