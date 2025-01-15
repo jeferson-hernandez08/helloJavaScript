fed9a292dd23c8e228dea756ccd74728c27abb93
Jeferson <jefer.hernandez1@gmail.com>
2025-01-14 21:02:33 -0500

    03-beginner-exercises inicio

---
+++ basic/03-beginner-exercises.js
@@ -0,0 +1,27 @@
+// 1. Escribe un comentario en una línea
+    // Hola esto es un comentario
+
+// 2. Escrie un comentario en varias lineas. 
+    /* 
+        Hola esto es
+        comentario
+        de varias
+        lineas
+    */
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+    // String 
+    let miNombre = "Jeferson Hernandez"
+
+    // Números
+    let miEdad   = 29      // Entero
+    let miAltura = 1.67   // Decimal
+
+    // Booleanos 
+    let soyIngeniero = true
+    let soyTecnologo = false
+
+    //
+    
+
+
