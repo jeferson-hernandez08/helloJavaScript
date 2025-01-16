2f99daeec0f5511a3fada20a92b0475518059173
Jeferson <jefer.hernandez1@gmail.com>
2025-01-15 19:28:27 -0500

    03-beginner-exercises, Realizado

--- basic/03-beginner-exercises.js
+++ basic/03-beginner-exercises.js
@@ -13,15 +13,217 @@
     // String 
     let miNombre = "Jeferson Hernandez"
 
-    // Números
+    // Number
     let miEdad   = 29      // Entero
     let miAltura = 1.67   // Decimal
 
-    // Booleanos 
-    let soyIngeniero = true
-    let soyTecnologo = false
+    // Boolean
+    let soyIngeniero = false
+    let soyTecnologo = true
 
-    //
+    // Indefined
+    let undefinedValue
+
+    // Null
+    let nullValue = null
+
+    // Symbol
+    let mySymbol = Symbol("Mysymbol")
+
+    // BigInt
+    let myBigInt  = BigInt(3434325254354678979789879789)
+    let myBigInt2 = 32454356458973475387583475489358937n
+
+// 4. Imprime por consola el valor de todas las variables
+    console.log(miNombre)
+    console.log(miEdad)
+    console.log(miAltura)
+    console.log(soyIngeniero)
+    console.log(soyTecnologo)
+    console.log(undefinedValue)
+    console.log(nullValue)
+    console.log(mySymbol)
+    console.log(myBigInt)
+    console.log(myBigInt2)
+    console.log("_____________________________________")
+
+// 5. Imprime por consola el tipo de todas variables
+    console.log(typeof miNombre)           // string
+    console.log(typeof miEdad)             // number
+    console.log(typeof miAltura)           // number
+    console.log(typeof soyIngeniero)       // boolean
+    console.log(typeof soyTecnologo)       // boolean
+    console.log(typeof undefinedValue)     // undefined
+    console.log(typeof nullValue)          // null | object
+    console.log(typeof mySymbol)           // symbol
+    console.log(typeof myBigInt)           // bigint
+    console.log(typeof myBigInt2)          // bigint
+    console.log("_____________________________________")
+
+// 6. A continuación modifica los valores de las variables por otros del mismo tipo
+    // String 
+    miNombre = "Sebastian Hernandez"
+
+    // Number
+    miEdad   = 32      // Entero
+    miAltura = 1.68   // Decimal
+
+    // Boolean
+    soyIngeniero = true
+    soyTecnologo = true
+
+    // Indefined
+    undefinedValue = 0
+
+    // Null
+    nullValue = null
+
+    // Symbol
+    mySymbol = Symbol("Sebitas Gordito")
+
+    // BigInt
+    myBigInt  = BigInt(33333333333333333333333333333)
+    myBigInt2 = 222222222222222222222222222222222222n
+
+    console.log(miNombre)
+    console.log(miEdad)
+    console.log(miAltura)
+    console.log(soyIngeniero)
+    console.log(soyTecnologo)
+    console.log(undefinedValue)
+    console.log(nullValue)
+    console.log(mySymbol)
+    console.log(myBigInt)
+    console.log(myBigInt2)
+    console.log("_____________________________________")
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+    // String 
+    miNombre = 1992
+
+    // Number
+    miEdad   = "32"      // Entero
+    miAltura = "1.68"   // Decimal
+
+    // Boolean
+    soyIngeniero = "si"
+    soyTecnologo = "si"
+
+    // Indefined
+    undefinedValue = 1.992
+
+    // Null
+    nullValue = null
+
+    // Symbol
+    mySymbol = Symbol(1992)
+
+    // BigInt
+    myBigInt  = BigInt(33333333333333333333333333333)
+    myBigInt2 = 222222222222222222222222222222222222n
+
+    console.log(miNombre)
+    console.log(miEdad)
+    console.log(miAltura)
+    console.log(soyIngeniero)
+    console.log(soyTecnologo)
+    console.log(undefinedValue)
+    console.log(nullValue)
+    console.log(mySymbol)
+    console.log(myBigInt)
+    console.log(myBigInt2)
+    console.log("_____________________________________")
+    /*
+        Podemos que al modificar las variales por otro tipo de variable No sale error 
+        Debido a que JavasScript es debilmente tipado. 
+     */
+
+// 8. Declara constantes con valores asociados a todos los tipos los tipos de datos primitivos.
+    // String 
+    const nombrePadre = "Edgar Hernandez"
+
+    // Number
+    const edadPadre  = 62      // Entero
+    const alturaPadre = 1.75 // Decimal
+
+    // Boolean
+    const Policia   = true
+    const ingeniero = false
+
+    // Indefined
+    //const undefined
+
+    // Null
+    const valorNulo = null
+
+    // Symbol
+    const apodoPadre = Symbol("Piolin")
+
+    // BigInt
+    const numeroLargo  = BigInt(11111111111111111111111111)
+    const numeroLago2 = 22222222222222222222222222222222222n
+
+    console.log(nombrePadre)
+    console.log(edadPadre)
+    console.log(alturaPadre)
+    console.log(Policia)
+    console.log(ingeniero)
+    // console.log(undefinedValue)
+    console.log(valorNulo)
+    console.log(apodoPadre)
+    console.log(numeroLargo)
+    console.log(numeroLago2)
+    console.log("_____________________________________")
+
+// 9. A continuación, modifica los valores de las constantes 
+    // String 
+    //nombrePadre = "Edgar Hernandez Martinez" 
+
+    // Number
+    //edadPadre  = 65      // Entero
+    //alturaPadre = 1.80   // Decimal
+
+    // Boolean
+    //Policia   = false
+    //ingeniero = true
+
+    // Indefined
+    //const undefined
+
+    // Null
+    //valorNulo = 0
+
+    // Symbol
+    //apodoPadre = Symbol("Tato")
+
+    // BigInt
+    //numeroLargo  = BigInt(2222222222222222222222222222)
+    //numeroLago2 = 3333333333333333333333333333333333333n
+
+    console.log(nombrePadre)
+    console.log(edadPadre)
+    console.log(alturaPadre)
+    console.log(Policia)
+    console.log(ingeniero)
+    // console.log(undefinedValue)
+    console.log(valorNulo)
+    console.log(apodoPadre)
+    console.log(numeroLargo)
+    console.log(numeroLago2)
+    console.log("_____________________________________")
+
+// 10. Comenta las lineas que produzca algún tipo de error al ejecutarse  
+    //  Se comentan todas las variables de las varaibbles const, sale error en todas ya que que const nunca puede cambiar.
+
+    
+
+
+     
+
+
+
+
+
     
 
 
