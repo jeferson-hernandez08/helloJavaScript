2f98524015e3a1bd0cc7f365c69eb58a47b0faa3
Jeferson <jefer.hernandez1@gmail.com>
2025-01-17 14:24:15 -0500

    04-operators, Inicio

---
+++ basic/04-operators.js
@@ -0,0 +1,28 @@
+/* Operadores: Los operadores se usan para realizar operaciones con los valores de las variables.
+   Se clasifican en diferentes categorias segun la funcionabilidad. 
+*/
+
+// Operadores Aritméticos: Son los operadores que ya conocemos, suma, resta, multiplicaion y division. 
+
+let a = 5
+let b = 10 
+
+console.log(5 + 10)   
+console.log(a + b)    // Suma
+console.log(a - b)    // Resta
+console.log(a * b)    // Multiplicación
+console.log(a / b)    // División
+
+console.log(a % b)    // Módulo
+console.log(a ** b)   // Exponente
+
+a++   // Incremento
+console.log(a)
+
+b--   // Decremento
+console.log(b)
+
+// Operadores de asignación: 
+
+
+
