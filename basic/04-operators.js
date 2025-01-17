06e63123e48d483a6b1e5f4637323b91c413a091
Jeferson <jefer.hernandez1@gmail.com>
2025-01-17 18:51:54 -0500

    04-operators, Continuación

--- basic/04-operators.js
+++ basic/04-operators.js
@@ -3,7 +3,6 @@
 */
 
 // Operadores Aritméticos: Son los operadores que ya conocemos, suma, resta, multiplicaion y division. 
-
 let a = 5
 let b = 10 
 
@@ -22,7 +21,31 @@ console.log(a)
 b--   // Decremento
 console.log(b)
 
-// Operadores de asignación: 
+// Operadores de asignación: Es cuando aisgnamos algun valor a una variable 
+let myVariable = 2
+console.log(myVariable)
+
+myVariable = myVariable + 2
+myVariable += 2              // Podemos ver que es lo mismo que myVariable = myVariable + 2, += 2 es la manera profesional y rapida.
+console.log(myVariable)
+
+/*  Nosotros podemos usar estos operadores de asignacion para hacer la resta, multiplicación, division,
+   Modulo y exponente. Son variaciones sobre las diferentes operaciones aritmeticas.
+*/
+
+myVariable -= 2  
+myVariable *= 2  
+myVariable /= 2  
+myVariable %= 2  
+myVariable **= 2  
+
+// Operadores de comparación: Como el nombre lo indica para comparar valores.  
+/*  En la leccion anterior vimos los tipo de dato booleano 
+*/
+
+
+
+
 
 
 
