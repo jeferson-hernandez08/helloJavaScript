ff72b74aae23887f4e70a4f78138f4dcd4e97b01
Jeferson <jefer.hernandez1@gmail.com>
2025-01-22 17:08:47 -0500

    05-operators-exercises, exercise 01 OK

--- basic/05-operators-exercises.js
+++ basic/05-operators-exercises.js
@@ -1,1 +1,69 @@
+
 // 1. Crea una variable para cada operación aritmetica.
+console.log("___________________________Respuesta Pregunta 1_____________________________________")
+console.log("******Primera Forma******")
+let a = 3
+let b = 2
+let sum = a + b       //Tercera Forma : Pendiente a realizar..
+
+console.log(a + b)    // Suma
+console.log(a - b)    // Resta
+console.log(a * b)    // Multiplicación
+console.log(a / b)    // División
+console.log(a % b)    // Modulo
+console.log(a ** b)   // Exponente
+a++                   // Incremento
+console.log(a)
+b++                   // Decremento
+console.log(b)
+
+console.log("******Segunda Forma******")
+// Suma
+let suma = 5 + 3  // 5 + 3 = 8
+
+// Resta
+let resta = 10 - 4  // 10 - 4 = 6
+
+// Multiplicación
+let multiplicacion = 7 * 2  // 7 * 2 = 14
+
+// División
+let division = 20 / 4  // 20 / 4 = 5
+
+// Módulo
+let modulo = 10 % 3  // 10 % 3 = 1 (residuo de la división 10 entre 3)
+
+// Exponente
+let exponente = 3 ** 2  // 3 ** 2 = 9 
+
+//Incremento
+let incremento = 2
+incremento++
+
+// Decremento
+let decremento = 2
+decremento--
+
+console.log("Suma:", suma)
+console.log("Resta:", resta)
+console.log("Multiplicación:", multiplicacion)
+console.log("División:", division)
+console.log("Módulo:", modulo)
+console.log("Exponente:", exponente)
+console.log("Incremento:", incremento)
+console.log("Decremento:", decremento)
+
+console.log("******Tercera Forma******")
+let c = 5
+let d = 2
+
+sum  = c + d
+res  = c - d
+mult = c * d
+
+console.log("Suma:", sum)
+console.log("Resta:", res)
+console.log("Resta:", mult)
+
+
+
