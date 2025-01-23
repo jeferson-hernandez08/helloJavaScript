65516d2e2b470afd78cc06605cfc83fee5fd3acf
Jeferson <jefer.hernandez1@gmail.com>
2025-01-23 14:54:07 -0500

    05-operators-exerises, ejercicio 2 OK

--- basic/05-operators-exercises.js
+++ basic/05-operators-exercises.js
@@ -4,7 +4,6 @@ console.log("___________________________Respuesta Pregunta 1____________________
 console.log("******Primera Forma******")
 let a = 3
 let b = 2
-let sum = a + b       //Tercera Forma : Pendiente a realizar..
 
 console.log(a + b)    // Suma
 console.log(a - b)    // Resta
@@ -14,7 +13,7 @@ console.log(a % b)    // Modulo
 console.log(a ** b)   // Exponente
 a++                   // Incremento
 console.log(a)
-b++                   // Decremento
+b--                   // Decremento
 console.log(b)
 
 console.log("******Segunda Forma******")
@@ -57,13 +56,139 @@ console.log("******Tercera Forma******")
 let c = 5
 let d = 2
 
-sum  = c + d
-res  = c - d
-mult = c * d
+sum   = c + d
+res   = c - d
+mult  = c * d
+divi  = c / d
+mod   = c % d
+expo  = c ** d
+c++
+d--
 
 console.log("Suma:", sum)
 console.log("Resta:", res)
-console.log("Resta:", mult)
+console.log("Multiplicación:", mult)
+console.log("División", divi)
+console.log("Modulo:", mod)
+console.log("Exponente:", expo)
+console.log("Incremento:", c)
+console.log("Decremento:", d)
+
+// 2. Crea una variable para cada tipo de operación de asignación, que haga uso de las variables utilizadas para 
+//    las operaciones aritmeticas. 
+console.log("___________________________Respuesta Pregunta 2_____________________________________")
+console.log("******Primera Forma******")
+// Asignacion simple
+let variable = 2
+
+// Asignacion suma
+let variableSuma = 2
+
+//Asignacion resta
+let variableResta = 2
+
+// Asignacion multiplicación
+let variableMultiplic = 2
+
+// Asignación división
+let variableDivision = 2
+
+// Asignación Modulo
+let variableModulo = 2
+
+//Asignación Exponente
+let variableExponente = 2
+
+variableSuma      += 2
+variableResta     -= 2
+variableMultiplic *= 2
+variableDivision  /= 2
+variableModulo    %= 2
+variableExponente **= 2
+
+console.log("Asignación Simple:", variable)
+console.log("Asignación Suma:", variableSuma)
+console.log("Asignación Resta:", variableResta)
+console.log("Asignación Multiplicación:", variableMultiplic)
+console.log("Asignación División:", variableDivision)
+console.log("Asignación Modulo:", variableModulo)
+console.log("Asignación Exponente:", variableExponente)
+
+console.log("******Segunda Forma******")
+// Usamos de las variables utilizadas de las operaciones aritmeticas ejercicio 1, de la tercera forma. 
+// Asignación Simple 
+let variableSimple = sum    
+
+// Asignación Suma
+let variableSum = sum
+
+// Asignación Resta
+let variableRes = res
+
+// Asignación Multiplicación
+let variableMult = mult
+
+// Asignación Dvisión
+let variableDivi = divi
+
+// Asignación Modulo
+let variableMod = mod
+
+// Asignación Exponente
+let variableExpo = expo
+
+variableSum  += 2
+variableRes  -= 2
+variableMult *= 2
+variableDivi /= 2
+variableMod  %= 2
+variableExpo **= 2
+
+console.log("Asignación Simple:", variableSimple)
+console.log("Asignación Suma:", variableSum)
+console.log("Asignación Resta:", variableRes)
+console.log("Asignación Multiplicación:", variableMult)
+console.log("Asignación División:", variableDivi)
+console.log("Asignación Modulo:", variableMod)
+console.log("Asignación Exponente:", variableExpo)
+
+console.log("******Tercera Forma******")
+// Usamos de las variables utilizadas de las operaciones aritmeticas ejercicio 1, de la segunda forma. 
+// Asignación simple
+let resultado = suma;  // Asigna el valor de la suma a resultado, // 5 + 3 = 8
+
+// Asignación Suma
+resultado += resta;  // resultado = resultado + resta, // 10 - 4 = 6
+
+// Asignación Resta
+resultado -= multiplicacion;  // resultado = resultado - multiplicacion, // // 7 * 2 = 14
+
+// Asignación Multiplicación
+resultado *= division;  // resultado = resultado * division, // 20 / 4 = 5
+
+// Asignación División
+resultado /= modulo;  // resultado = resultado / modulo, // 10 % 3 = 1
+
+// Asignación Módulo
+resultado %= exponente;  // resultado = resultado % exponente, // // 3 ** 2 = 9
+
+// Asignación Exponente
+resultado **= incremento;  // resultado = resultado ** incremento
+
+console.log("Resultado final:", resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
