e80e991222fe19c0a958689e627882a04baac3a9
Jeferson <jefer.hernandez1@gmail.com>
2025-01-28 12:00:25 -0500

    07-strings-exercises, Inicio

---
+++ basic/07-strings-exercises.js
@@ -0,0 +1,28 @@
+// 1. Concatena dos cadenas de texto
+let myName  = "Jeferson Hernandez"
+let myName2 = "Sebastian Hernandez"
+let alias1  = "Kuki"
+let alias2  = "Gordito"
+
+let greeting1 = "Hola, mi nombre es " + myName + " me llaman " + alias1 + " y quiero ser un gran programador"
+let greeting2 = "Hola, mi nombre es " + myName2 + " me llaman " + alias2 + " y soy un gran programador"
+console.log(greeting1)
+console.log(greeting2)
+
+// 2. Muestra la longitud  de una cadena de texto.
+
+// 3. Muestra el primer y último carácter de un string 
+
+// 4. Convierte a mayúsculas y minúsculas un string
+
+// 5. Crea una cadena de texto en varias lineas
+
+// 6. Interpola el valor de una variable en un string 
+
+// 7. Reemplaza todos los espacios en blanco de un string por guiones 
+
+// 8. Comprueba si una cadena de texto contiene una palabra concreta
+
+// 9. Comprueba si dos strings son iguales 
+
+// 10. Comprueba si dos strings tienen la misma longitud
