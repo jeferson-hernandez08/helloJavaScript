409eb195966f1d4d35065561c4a05fef064c29e3
Jeferson <jefer.hernandez1@gmail.com>
2025-01-30 15:50:16 -0500

    08-conditionals, Finalizado

--- basic/08-conditionals.js
+++ basic/08-conditionals.js
@@ -86,7 +86,7 @@ console.log("____________________________Switch_________________________________
     Es util cuando tenemos muchas condiciones que verificar pero contra una misma variable.
 */
 console.log("******Forma Clasica******")
-let day = 0
+let day = 3
 let dayName
 
 if (day == 0) {
@@ -96,14 +96,16 @@ if (day == 0) {
 } else if (day == 2) {
     dayName = "Miercoles"
 } else if (day == 3) {
-    dayName == "Jueves"
+    dayName = "Jueves"
 } else if (day == 4) {
     dayName = "Viernes"
 } else if (day == 5) {
     dayName = "Sabado"
 } else if (day == 6) {
     dayName = "Domingo"
-} 
+} else {
+    dayName = "Numero de día incorrecto"
+}
 console.log(dayName)
 /*  Podemos ver que de la forma clasica es muy poco optimo y es muy poco operativo, lo que es 
     un poco confundido entenderlo por que siempre lo que estamos inspeccionando siempre es la variable day
@@ -113,24 +115,46 @@ console.log(dayName)
 */
 
 console.log("******Forma con switch******")
-switch (day) {              // Aquí ponemos cual es la variable que tenemos inspeccionar o validar en este caso day
-    case 0:                 // Este case es donde va a estar la condición en este caso dia sea 0, y ejecutamos nuestro bloque validado ene este caso Lunes.
+switch (day) {                // Aquí ponemos cual es la variable que tenemos inspeccionar o validar en este caso day
+    case 0:                   // Este case es donde va a estar la condición en este caso dia sea 0, y ejecutamos nuestro bloque validado ene este caso Lunes.
         dayName = "Lunes"
-    case 1:                 // Y así sucesivamente validamos una por una. 
+        break                 // Cuando usemos la instrucción case debemos finalizarla con break, reak significa romper, rompe la ejecución del bloque, se sale del bloque de codigo.
+    case 1:                   // Y así sucesivamente validamos una por una. 
         dayName = "Martes"
+        break
     case 2:
         dayName = "Miercoles"
+        break
     case 3:
         dayName = "Jueves"
+        break
     case 4:
         dayName = "Viernes"
+        break
     case 5:
         dayName = "Sabado"
+        break
     case 6:
         dayName = "Domingo"
+        break
+    default:                   //Default es el else de toda la estructura
+        dayName = "Numero de día incorrecto"
+        //break                 // Meter el break aquí en el default es opcional por que ya ejecuta mas cosas.
 }
 console.log(dayName)
 /*  Podemos ver que en lugar de hacer de if que son muy redundantes y largos y si encontramos que la variable que revisamos
-    es la misma lo hacemos un switch y e suna forma mucho mas optima
+    es la misma lo hacemos un switch y e suna forma mucho mas optima.
+*/
+/*  En algunos casos va a tener una ventajas y desventajas entre el if y el switch:
+    El Switch Ventajas y Desventajas: 
+    Ventajas:
+    1. El switch es muy legible se entiende super rapido lo que queremos hacer, por que estamos revisando
+    es lo que está en mismo switch, si loq ue validamos en switch es igual alguno de los valores que nsotros metemos en los cases.
+    2. Puede ser muy eficente por que JS se encarga de optimizarlo para que esta ejecución del switch se ejecute mucho mas rapido 
+    que si metemos ese if por el medio.
+    Desventajas:
+    1. Es menos flexible, si nosotros queremos meter coondiciones complejas como el en el if o else if por ejemplo
+    en la lección anterior (5 > 10 && 15 > 20 || 30 < 40) en el switch no se puede hacer.
+    2. Debemos meterle el break si o si de lo contrario no funciona y nos sale error.
 */
 
