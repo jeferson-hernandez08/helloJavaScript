a9495954c8b021b6e6cd0bb9f1f5bb79b0dacc05
Jeferson <jefer.hernandez1@gmail.com>
2025-01-30 13:14:57 -0500

    08-conditionals, Continuación

--- basic/08-conditionals.js
+++ basic/08-conditionals.js
@@ -9,20 +9,33 @@
 
 // if, else if, else
 
+console.log("____________________________If (si)____________________________________")
 // if (si): Significa si pasa esto haga esto. La estructura if ejecuta un bloque de código si una condición es verdadera. 
-let age = 10
+let age = 37
 
 if (age == 37) {
     console.log("La edad es 37")        // Si se cumple, va ejecutar el cosigo que esta en la llaves.
 }
 
+console.log("____________________________Else (si no)____________________________________")
 // else (si no): Significa si no, else ejecuta un bloque de código si una condición es falsa. 
 if (age == 37) {
-    console.log("La edad es 37")       
+    console.log("La edad es 37")
 } else {
     console.log("La edad No es 37")
 }
 
+console.log("******Otra Forma******")
+let mensaje
+
+if (age == 37) {
+    mensaje = "La edad es 37"
+} else {
+    mensaje = "La edad No es 37"
+}
+console.log(mensaje)
+
+console.log("____________________________Else if (si no, si)____________________________________")
 // else if (si no, si)
 if (age == 37) {
     console.log("La edad es 37")       // No se cumple esta condicionn pasa a evaluar la siguiente condición y así sucesivamente
@@ -31,3 +44,93 @@ if (age == 37) {
 } else {
     console.log("La edad No es 37 ni es menor de edad")
 }
+/*  Tenemos tres 
+    Si el primer if se cumple se ejecuta el codigo si la edad es 37, console.log("La edad es 37") .
+    Si la edad no es 37 va a validar el sugundo if si se cumple ejecuta el codigo, console.log("Es menor de edad")
+    Si no se cumple las validaciones de la edad 37 o la edad < 18, no ejecuta el codigo del else console.log("La edad No es 37 ni es menor de edad")
+    lo que significa el else que ninguna si ninguna de las dos condiciones anteriores es verdadera me ejecuta el codigo. 
+    Podemos todos los else if que queramos si queremos seguir validando mas cosas y podemos hacer condiciones mas complejas
+*/
+
+console.log("____________________________Operador Ternario____________________________________")
+//Operador Ternario: Es una forma mas rapida de escribir un if y un else. Una manera mas compacta de escribir un codicional simple.
+const message = age == 37 ? "La edad es 37" : "La edad No es 37"
+console.log(message)
+
+/*  El resultado de este este operador ternario lo podemos asignar a una variable.
+    Aqui estamos combinando dos cosas la parte de operadores y la aparte de condicionales
+    Este ejemplo de operador ternario es igual que hacer esto:
+    let mensaje
+
+    if (age == 37) {
+        mensaje = "La edad es 37"
+    } else {
+        mensaje = "La edad No es 37"
+    }
+    console.log(mensaje)
+
+    El resultado lo podemos asignar a una variable o una constante y despues podemos trabajar con ese resultado
+    Lo cual asignamos el resultado ternario a esa constante message
+    Los operadores ternarios mucha veces se usan para operaciones simples.
+*/
+
+/*  Recordemos que el operador ternario va primero la validación del if, age == 37 ?, si se cumple va antes
+    de los dos puntos el bloque de código y si no se cumple va después de los dos puntos el otro bloque de codigo else.
+    Recordemos que lo podemos reprentar de esta forma: 
+    age == 37 ? console.log("La edad es 37") : console.log("La edad No es 37")
+*/
+
+console.log("____________________________Switch____________________________________")
+// Switch: Switch es una alternativa de anidar if else, cuando por ejemplo tenemos que metar muchos else if y else if, etc. 
+/*  El Switch unicamente se usa cuando tenemos la necesidad de comparar una unica variable con varios valores posibles, 
+    Es util cuando tenemos muchas condiciones que verificar pero contra una misma variable.
+*/
+console.log("******Forma Clasica******")
+let day = 0
+let dayName
+
+if (day == 0) {
+    dayName = "Lunes"
+} else if (day == 1) {
+    dayName = "Martes"
+} else if (day == 2) {
+    dayName = "Miercoles"
+} else if (day == 3) {
+    dayName == "Jueves"
+} else if (day == 4) {
+    dayName = "Viernes"
+} else if (day == 5) {
+    dayName = "Sabado"
+} else if (day == 6) {
+    dayName = "Domingo"
+} 
+console.log(dayName)
+/*  Podemos ver que de la forma clasica es muy poco optimo y es muy poco operativo, lo que es 
+    un poco confundido entenderlo por que siempre lo que estamos inspeccionando siempre es la variable day
+    todas las condicionan son de la misma variable donde siempre inspeccionamos la misma variable.
+    Para eso tenemos el switch, lo cual es una alternativa para cuando tenemos que revisar condiciones que se verifican 
+    contra una misma variable.
+*/
+
+console.log("******Forma con switch******")
+switch (day) {              // Aquí ponemos cual es la variable que tenemos inspeccionar o validar en este caso day
+    case 0:                 // Este case es donde va a estar la condición en este caso dia sea 0, y ejecutamos nuestro bloque validado ene este caso Lunes.
+        dayName = "Lunes"
+    case 1:                 // Y así sucesivamente validamos una por una. 
+        dayName = "Martes"
+    case 2:
+        dayName = "Miercoles"
+    case 3:
+        dayName = "Jueves"
+    case 4:
+        dayName = "Viernes"
+    case 5:
+        dayName = "Sabado"
+    case 6:
+        dayName = "Domingo"
+}
+console.log(dayName)
+/*  Podemos ver que en lugar de hacer de if que son muy redundantes y largos y si encontramos que la variable que revisamos
+    es la misma lo hacemos un switch y e suna forma mucho mas optima
+*/
+
