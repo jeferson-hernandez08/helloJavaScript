0cf9e84ee02385205ac1ba6e681c84307d14535e
Jeferson <jefer.hernandez1@gmail.com>
2025-02-03 18:39:24 -0500

    10-arrays, Continuación

--- basic/10-array.js
+++ basic/10-array.js
@@ -1,1 +1,18 @@
-// Arrays
+/*  Arrays: 
+    Los arrays tambien son llamados "Estructuras de datos", recordemos que anteriormente vimos todos los tipos de 
+    datos primitivos (strings, numbers, booleans, undefined, symbol, bigint). 
+    Las estructuras de datos nos permiten almacenar varios valores en una sola variable, anteriormente creabamos una 
+    variable y le asignabamos un valor sea strings, numerico, bolleano, etc . Pero si queremos asignar  auna misma variable
+    varios o un conjunto de datos para eso tenemos diferentes estructuras de datos o arrays. 
+    Hay tres tipos de esturas de datos o arrays que vamos a ver a continuación. 
+*/
+
+// Arrays: También llamadas listas, son utiles cuando necesitamos manejar una lista de elementos sea numeros, textos, etc. 
+let myArray = []             // Aqui definimos una estructura de datos que nos permite almacenar un listado de datos.
+let myArray2 = new Array()   // Otra forma de crear un array
+
+console.log(myArray)
+console.log(myArray2)
+/*  
+    Estas dos formas son iguales, pero es recomendado usar la primera forma
+*/
