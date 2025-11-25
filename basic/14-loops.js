37892743e1d523b6cad4afa53f1a4e71a574175b
Jeferson <jefer.hernandez1@gmail.com>
2025-11-24 19:12:50 -0500

    14-loops, Start loops

---
+++ basic/14-loops.js
@@ -0,0 +1,33 @@
+/*  Loops o bucles: 
+    Son estructuras de control, que nos sirve para repetir un bloque de código mientras una 
+    condición sea verdadera (De otra forma un while que incrementa), de otra froma es un forma indirecta
+    de aplicar tambien condiciones.
+    Son bucles son esenciales para realizar tareas repetitivas, para automatizar procesos y para manejar
+    en muchos casos grades cantidades de datos y de una manera mas eficiente. Por eso los bucles se asocian 
+    tambien a las estructuras de datos al array, al set o al map. Pero no siempre los blucles estan ligados 
+    a ellos.
+    En resumen un loop es una estructura de control que permite repetir un bloque de codigo.
+    si por ejemplo queremos hacer varias veces algo con un loop es la manera mas eficiente que tenemos en 
+    los lenguajes de programacion trabajar con esta repetición. 
+
+    Un loop (o bucle) es una estructura de control que permite repetir un bloque de código varias veces 
+    de manera automática.
+    ¿Para qué se usa un loop?
+        Se usa para automatizar tareas repetitivas, como:
+        Recorrer elementos de un array.
+        Ejecutar un proceso muchas veces.
+        Buscar información dentro de una colección.
+        Realizar cálculos repetitivos.
+        Procesar datos uno por uno.
+
+*/
+
+// For: For lo que hace es ejecutarse con una condicion que se cada vez que realiza una iteración, si la condicion es true se va seguir ejecutando y si pasa a false del loop deja de ejecutarse. 
+console.log("____________________________For____________________________________")
+for (let i = 0; i < 5; i++) {
+    console.log("Hola");
+}
+
+
+// Comentario quede en : 03:17:50 min Bucles
+
