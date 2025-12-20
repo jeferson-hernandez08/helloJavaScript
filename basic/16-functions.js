2530e0a2864fd713666a82dc1acf7dcd0972a947
Jeferson <jefer.hernandez1@gmail.com>
2025-12-19 19:26:51 -0500

    16-functions theme

---
+++ basic/16-functions.js
@@ -0,0 +1,43 @@
+/*  Funciones:
+    Una función es un bloque de código diseñado para realizar una tarea especifica, lo cual las funciones
+    las tenemos que llamar, las tenemos que invocar. Con las funciones vamos a poder a realizar algo
+    muy importante en programacion que es escribir un buen código, escribir código limpio. 
+    Una de las cosas fundamentales para escribir buen código es crear buen código que sea capaz de reutilizar, 
+    que sea capaz de modularizar, que seamos capaces de leer de una forma más simple, incluso de acabar 
+    manejando un programa en partes más simples.
+
+    Una función es un bloque de código reutilizable que realiza una tarea específica y que se ejecuta 
+    solo cuando la llamas. En pocas palabras: una función es una acción con nombre.
+    ¿Para qué se usa una función?
+        Las funciones se usan para:
+        Evitar repetir código
+        Organizar mejor el programa
+        Reutilizar lógica
+        Hacer el código más claro y mantenible
+        Separar responsabilidades
+
+    ¿Para qué sirve una función?
+        Ejecutar una tarea cuando se necesite
+        Recibir datos (parámetros)
+        Procesar información
+        Devolver un resultado
+*/
+
+// Funciones:
+console.log("____________________________Funciones____________________________________")
+console.log("******Funciones******")
+function myFunc() {
+    console.log("¡Hola, función!");
+}
+
+
+
+/*  
+    Creaamoa la palabra function y el nombre de una function es como las variables las creamos en camelCase,
+    una function debe tener un nombre representativo un nombre que entandamos
+    
+*/
+
+
+// Comentario quede en : 03:35: min Funciones
+
