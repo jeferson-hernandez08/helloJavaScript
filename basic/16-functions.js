6dc6fa3c1158538a49f85191d5f30b93a5743173
Jeferson <jefer.hernandez1@gmail.com>
2025-12-22 13:12:06 -0500

    16-functions, continued theme type of functions

--- basic/16-functions.js
+++ basic/16-functions.js
@@ -23,15 +23,16 @@
         Devolver un resultado
 */
 
-// Funciones:
-console.log("____________________________Funciones____________________________________")
+// Funciones Simple:
+console.log("____________________________Funciones Simple____________________________________")
 console.log("******Funciones******")
 function myFunc() {
     console.log("¡Hola, función!");
 }
 
-
-
+for (let i = 0; i < 5; i++) {
+  myFunc();
+}
 /*  
     - Creamos la palabra function
     - Creamos el nombre de una function, es como las variables las creamos en camelCase,
@@ -44,9 +45,32 @@ function myFunc() {
     Esta seria la función la funcion un bloque de código que realiza una tarea especifica, una función realiza
     una tarea especifica en este caso simplemente imprimir por consola. Lo que realmente hace especial una 
     función es que puede ser invocada. 
+    Donde para imprimir esta funcion debe ser invocada  o llamada.
+    - Tambien si queremos llamar a la funcion dentro de un for tambien los podemos hacer para imprimirlo 5 veces
     
 */
 
+// Con Parámetros: Los parámetros son variables que una función recibe para poder trabajar con datos. Son como entradas de la función.
+console.log("____________________________Funciones con Parámetros____________________________________")
+function myFuncWithParams(name) {
+    console.log(`¡Hola, ${name}!`);
+}
 
-// Comentario quede en : 03:36: min Funciones -- ..
+myFuncWithParams("Jeferson");    // Pasamos el dato del parámetro nombre
+myFuncWithParams("Sebastian");    // Pasamos el dato del parámetro nombre
+/* 
+  Los parámetros son variables, para pasar datos que la funcion recibe. Son como entradas de la función.
+  Podemos poner y usar multiples o muchos parametros.
+*/
+
+// Funciones Anónimas: Las funciones anonimas son funciones que no tienen un nombre definivo.
+console.log("____________________________Funciones con Anónimas____________________________________")
+
+/* 
+  Quiere decir que creamos una funcion donde le pasamos el nombre o parámetro que queramos y simplemente
+  meterle la ejecución.
+  Pero lo que pasa con las fucniones anonimas es que 
+*/
+
+// Comentario quede en : 03:39: min Funciones -- ..
 
